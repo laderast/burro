42b207e4cb8fd849e24e3078fc3196a3ae39215d
Ted Laderas <roshioshi@gmail.com>
2019-01-10 17:05:21 -0800

    update with docs folder

---
+++ docs/reference/figures/burro-hex.R
@@ -0,0 +1,6 @@
+library(hexSticker)
+
+sticker("~/Code/burro/VA11-150ppp-01.gif",
+        package="burro", filename="burro.png",
+        h_fill="#FFFFFF", p_size=20, s_width = 0.3,
+        s_height=0.3, s_x=1, p_color="#3498DB", h_color="#3498DB")
