a9e23591922e5e816c342b2cba5adf3583fe6166
Ted Laderas <tedladeras@gmail.com>
2020-07-01 18:55:18 +0000

    adding test

--- tests/testthat/test-get_categorical_data.R
+++ tests/testthat/test-get_categorical_data.R
@@ -7,9 +7,11 @@ data(diamonds)
 cats1 <- get_category_variables(iris)
 cats2 <- get_category_variables(diamonds)
 cats3 <- get_category_variables(candy_rankings)
+cats4 <- get_category_variables(mtcars)
 
 test_that("get category variables works", {
   expect_equal(length(cats1), 1)
   expect_equal(length(cats2), 3)
   expect_equal(names(cats1), "Species")
+  expect_equal(NULL, cats4)
 })
