a88a225b195000c2deafb5dd458bd95bec45fb91
Ted Laderas <roshioshi@gmail.com>
2020-07-03 13:40:43 -0700

    update with explore_data2 which uses flexdashboard

--- R/run_dashboard.R
+++ R/run_dashboard.R
@@ -1,4 +1,15 @@
-explore_data2 <- function(dataset, path="dashboard.Rmd", data_dictionary=NULL){
+#' Title
+#'
+#' @param dataset - dataset to explore
+#' @param data_dictionary
+#'
+#' @return
+#' @export
+#'
+#' @examples
+explore_data2 <- function(dataset, data_dictionary=NULL){
+
+  path=system.file("app/dashboard.Rmd", package="burro")
 
   dataset_name <- deparse(substitute(dataset))
   rmarkdown::run(path)
